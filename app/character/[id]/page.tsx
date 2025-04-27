1af37eadcd2ab4b3a78ffd7867be60614a878770
ArtemBedash <artembedash@TimonChik.local>
2025-04-27 16:22:14 +0300

    Add a button to return to the main page

--- app/character/[id]/page.tsx
+++ app/character/[id]/page.tsx
@@ -1,51 +1,43 @@
+import Link from "next/link";
 import Image from "next/image";
-import type {Metadata} from "next";
-
 
 type Props = {
-    params: Promise<{
-        id: string
-    }>
+    params: {
+        id: string;
+    };
+};
+
+async function fetchCharacter(id: string) {
+    const res = await fetch(`https://rickandmortyapi.com/api/character/${id}`, { cache: "no-store" });
+    return res.json();
 }
 
-async function getData(id: string) {
-    const response = await fetch(`https://rickandmortyapi.com/api/character/${id}`, {
-        cache: "no-store",
-    });
-
-    if (!response.ok) {
-        throw new Error("Failed to fetch character data");
-    }
-
-    return response.json();
-}
-
-export async function generateMetadata({params}: Props): Promise<Metadata> {
-    const {id} = await params;
-    const character = await getData(id);
-
-    return {
-        title: character.name,
-    }
-
-
-}
-
-
-
-export default async function CharacterPage({params}: Props)     {
-const {id} = await params
-    const character = await getData(id);
+export default async function CharacterPage({ params: { id } }: Props) {
+    const character = await fetchCharacter(id);
 
     return (
-        <div className="max-w-4xl mx-auto p-4">
-            <h1 className="text-3xl font-bold text-center mb-4">{character.name}</h1>
-            <Image src={character.image}
-                   alt={character.name}
-                   width={300}
-                   height={300}
-                   className="w-full h-auto rounded-lg shadow-lg mb-4"/>
-            <h2 className="text-xl text-center text-gray-600">{character.species}</h2>
+        <div className="min-h-screen flex flex-col items-center justify-center p-4 bg-gray-100">
+            <Link href="/">
+                <button
+                    type="button"
+                    className="mb-6 bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
+                    To The Main Page
+                </button>
+            </Link>
+
+            <div className="bg-white p-6 rounded-2xl shadow-xl w-full max-w-md text-center">
+                <Image
+                    src={character.image}
+                    alt={character.name}
+                    width={150} // меньше картинка
+                    height={150}
+                    className="mx-auto rounded-full mb-4"
+                />
+                <h1 className="text-2xl font-bold mb-2">{character.name}</h1>
+                <h2 className="text-lg text-gray-600">Species: {character.species}</h2>
+                <h2 className="text-lg text-gray-600">Location: {character.location.name}</h2>
+            </div>
         </div>
     );
 }
+
